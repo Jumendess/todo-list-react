0d59bfb6d3b72c38c62beccfaf672e559a7f2bb8
Julio Mendes <julio.mendes60@gmail.com>
2023-10-19 10:09:20 -0300

    subindo arquivos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 
 import './App.css';
 
-const App = () => {
-  <section id="app" className="container">
-    <header>
-      <h1 className="">Todo list</h1>;
-    </header>
-    <section className="main">
-      <input />
+function App() {
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState('');
+
+  const erase = () => {
+    setValue('');
+  };
+
+  const submit = () => {
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      },
+    ]);
+
+    erase();
+  };
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+  return (
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">todo</h1>
+      </header>
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="o que precisa ser feito?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className="todo">{todo.title}</span>
+              <button className="remove" type="button">
+                <MdDelete size={28} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
     </section>
-  </section>;
-};
+  );
+}
 
 export default App;
